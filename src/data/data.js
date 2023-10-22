bed4cf946ef2e3a455df0569136e200a1afd0c4d
heejung <dap072055@naver.com>
2023-10-22 21:23:44 +0900

    routine 페이지 추가

---
+++ src/data/data.js
@@ -0,0 +1,19 @@
+let data = [
+  {
+    id: 1,
+    name: "복부 운동 할 수 있어요!",
+    hits: "15",
+  },
+  {
+    id: 2,
+    name: "간단하게 두가지 하체운동",
+    hits: "24",
+  },
+  {
+    id: 3,
+    name: "어깨운동 한세트씩",
+    hits: "90",
+  },
+];
+
+export default data;
